68b36829ef211e737d783807f9c190052729f657
javatcoding1 <jaminmadhav1>
2024-12-13 14:36:47 +0530

    updated home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
-
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox"
 const Home = () => {
+  const loggedIn ={firstName:"Jayanth"}
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type="greetings"
+          title="Welcome"
+          user={loggedIn?.firstName||"Gust"}
+          subtext="Access and Manage your Account and Transactions efficiently"
+          />
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section> 
   )
 }
 
