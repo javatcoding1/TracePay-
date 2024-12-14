d1fb398863b83eb9092ab43ebb1676be00bf3e38
javatcoding1 <jaminmadhav1>
2024-12-14 11:18:43 +0530

    implemented the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import HeaderBox from "@/components/HeaderBox"
 import TotalBalanceBox from "@/components/TotalBalanceBox"
+import RightSidebar from "@/components/RightSidebar"
 const Home = () => {
-  const loggedIn ={firstName:"Jayanth"}
+  const loggedIn ={firstName:"Jayanth" , lastName:"Madhav" , email:"jayanthmadhav1@gmail.com"}
   return (
     <section className='home'>
       <div className="home-content">
@@ -19,7 +20,13 @@ const Home = () => {
           totalCurrentBalance={1250.35}
           />
         </header>
+        Recent Transctions
       </div>
+      <RightSidebar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{ currentBalance:123.50},{currentBalance:500.00}]}
+      />
     </section> 
   )
 }
